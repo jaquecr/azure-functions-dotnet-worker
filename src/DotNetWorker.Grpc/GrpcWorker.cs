c241779ec588c5de3820e02891ef6d17a635f858
Sarah Vu <sarahvu@microsoft.com>
2021-10-28 15:10:17 -0700

    clean up comments

--- src/DotNetWorker.Grpc/GrpcWorker.cs
+++ src/DotNetWorker.Grpc/GrpcWorker.cs
@@ -244,7 +244,6 @@ namespace Microsoft.Azure.Functions.Worker
 
             try
             {
-                // we need to get a list of items of type "FunctionLoadRequests" which has a field RpcFunctionMetadata
                 var functionMetadata = GetFunctionLoadRequests(directory);
 
                 for (int i = 0; i < functionMetadata.Count; i++)
